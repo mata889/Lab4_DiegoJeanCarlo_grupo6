7a31da57559e4b20d81da1e5acabf4d1fb0ae659
Jeancasoto <jeancasoto@yahoo.com>
2017-11-03 18:49:07 -0600

    algp]

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <string>
+#include "minesweeper.h"
+#include "player.h"
+#include <vector>
+
+bool recursivo(int** , int , int , int);
 
 using namespace std;
 int main(){
@@ -10,7 +15,7 @@ int main(){
 		cout<<"BIENVENIDO \n 1-Login \n 2-Crear usuario \n 3-Ver puntuaciones \n 4-Salir"<<endl<<endl;
 		cin>>opcion;
 		cout<<endl;
-//		vector<jugador> players;
+		vector<player> players;
 		switch(opcion){
 			case 1:{
 					string usuario;
@@ -38,7 +43,13 @@ int main(){
 					}
 					
 
-					cout<<endl<<endl;
+
+					//cout<<endl<<endl;
+
+					//minesweeper obj;					
+
+					//int** matrixnum = obj.CrearMatrixInt(n);
+					//obj.imprimirMatrix(matrixnum);
 
 
 				}// fin case 1
@@ -48,15 +59,19 @@ int main(){
 				string nombre;
 				string usuario;
 				string contra;
-
+				
+				
 				cout<<"Ingrese su nombre:"<<endl;
 				cin>>nombre;
 				cout<<"Ingrese su username:"<<endl;
 				cin>>usuario;
 				cout<<"Ingrese su password:"<<endl;
 				cin>>contra;
+				
+				player u = player(nombre, usuario, contra);
 
-//				players.push_back(usuario);
+
+				players.push_back(u);
 			   
 			   }// fin case 2
 			   break;
@@ -81,3 +96,60 @@ int main(){
 
 	return 0;
 }
+
+bool recursiva(int** m, int x, int y, int size){
+	//	printMatrix(m,size);
+		cout<<endl;
+		cin.ignore();
+				
+		if(y==size-1){
+			m[x][y]=9;
+			cout<<"PERDIO"<<endl;
+			return true;
+			}else{
+														
+				bool salio = false;
+				m[x][y]=9 ;
+
+				//arriba
+			if(x>0 && !salio){
+					if(m[x-1][y]== 0){
+				salio=recursiva(m, size, x-1, y);
+				}
+			}
+
+			//derecha
+			if(y+1 < size && !salio){
+				if(m[x][y+1]==0){
+				salio= recursiva(m, size, x, y+1);
+					}
+				}
+
+			//abajo
+		if(x+1 <size && !salio){
+			if(m[x+1][y]==0){
+			salio=recursiva(m, size, x+1, y);
+					}
+			}
+
+		//izquierda
+			if(y>0 && !salio){
+
+			if(m[x][y-1]==0){
+		salio=recursiva(m, size, x, y-1);
+					}
+					}
+																											
+			if(salio){
+					m[x][y]==0;
+				return salio;
+			}
+
+																												
+																												
+		}
+
+
+
+}//fin de la recursiva
+																				
